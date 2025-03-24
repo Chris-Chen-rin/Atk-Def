7e4accf2b12db4db6b22021bcfaf241308a2acd1
Chris-Chen-rin <76683292+Chris-Chen-rin@users.noreply.github.com>
2025-03-25 04:46:28 +0800

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,13 +1,22 @@
-let vc = localStorage.getItem('visitCount');
-
-if (vc === null) {
-  vc = 1;
-} else {
-  vc = parseInt(vc) + 1;
+document.addEventListener("DOMContentLoaded", function (){
+  let vc = localStorage.getItem('visitCount');
+  
+  if (vc === null) {
+    vc = 1;
+  } else {
+    vc = parseInt(vc) + 1;
+  }
+  
+  // 更新 localStorage
+  localStorage.setItem('visitCount', vc);
+  
+  // 顯示訪問次數
+  // document.getElementById('visitCounter').textContent = visitCount;
+  let counterElement = document.getElementById('visitCounter');
+  if (counterElement) {
+      counterElement.textContent = visitCount;
+  } else {
+      console.error("錯誤：找不到 ID 為 'visitCounter' 的元素！");
+  }
 }
 
-// 更新 localStorage
-localStorage.setItem('visitCount', vc);
-
-// 顯示訪問次數
-document.getElementById('visitCounter').textContent = visitCount;
