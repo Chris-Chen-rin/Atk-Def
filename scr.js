823e96bc0f3b20804cbc168980f19583c77cdde6
Chris-Chen-rin <76683292+Chris-Chen-rin@users.noreply.github.com>
2025-03-25 05:22:01 +0800

    Update scr.js

--- scr.js
+++ scr.js
@@ -15,13 +15,7 @@ function update(){
   localStorage.setItem('visitCount', vc);
   
   // 顯示訪問次數
-  // document.getElementById('visitCounter').textContent = visitCount;
-  let counterElement = document.getElementById('visitCounter');
-  if (counterElement) {
-      counterElement.textContent = visitCount;
-  } else {
-      console.error("錯誤：找不到 ID 為 'visitCounter' 的元素！");
-  }
+  document.getElementById('visitCounter').textContent = visitCount;
 }
 
 // hello?
