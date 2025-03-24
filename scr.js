8df954340e8ae7d0951c3e0d81bd5c04a7ff2426
Chris-Chen-rin <76683292+Chris-Chen-rin@users.noreply.github.com>
2025-03-25 03:51:20 +0800

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,1 +1,13 @@
-document.getElementById("demo").innerHTML = "Hello World!";
+let visitCount = localStorage.getItem("visitCount");
+
+if (visitCount === null) {
+  visitCount = 1;
+} else {
+  visitCount = parseInt(visitCount) + 1;
+}
+
+// 更新 localStorage
+localStorage.setItem("visitCount", visitCount);
+
+// 顯示訪問次數
+document.getElementById("visitCounter").textContent = visitCount;
